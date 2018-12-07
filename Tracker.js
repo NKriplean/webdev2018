a6c151288986d0b4c56cf9b9047b1ed6a907365d
NKriplean <kripln55@uwosh.edu>
2018-12-07 13:24:46 -0600

    Timer started

--- Tracker.js
+++ Tracker.js
@@ -12,3 +12,24 @@ $(function() {
 $('.progressbar li.active:before').on('click', '#tracker' function(event){
 	$(event.delegateTarget).css('border-color': 'green');
 });
+
+
+function progessTimer(){
+	var timeleft = 30;
+	var downloadTimer = setInterval(function(){
+	timeleft--;
+	document.getElementById("countdowntimer").textContent = timeleft;
+	if(timeleft <=20)
+	{
+		document.getElementById("")
+	}
+	else if(timeLeft <= 10)
+	{
+		
+	}
+	else if(timeleft <= 0)
+	{
+		//End of tracker
+	}
+	},delayTime);
+}
