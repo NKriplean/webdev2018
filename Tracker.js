7a4a0d1f3f741fea09f0d0d92c76eac0a41d6e4c
NKriplean <kripln55@uwosh.edu>
2018-12-09 13:46:49 -0600

    Updated html and js for tracker

--- Tracker.js
+++ Tracker.js
@@ -1,35 +1,41 @@
-(// JavaScript source code
+// JavaScript source code
 var delayTime = 2000; // milliseconds
 
-$(function() {
-  $('#tracker').click(function () {
-    setTimeout(function () {
-      $('.progressbar li.active:before').css({"border-color": "green"});
-    }, delayTime);
-  });
-});
+//$(function() {
+//  $('#tracker').click(function () {
+//    setTimeout(function () {
+//      $('.progressbar li.active:before').css({"border-color": "green"});
+//    }, delayTime);
+//  });
+//});
 
-$('.progressbar li.active:before').on('click', '#tracker' function(event){
+//$('.progressbar li.active:before').on('click', '#tracker' function(event){
 	$(event.delegateTarget).css('border-color': 'green');
-});
+//});
 
 
-function progessTimer(){
+function progressTimer(){
 	var timeleft = 30;
+	alert("hello");
 	var downloadTimer = setInterval(function(){
-	timeleft--;
-	document.getElementById("countdowntimer").textContent = timeleft;
-	if(timeleft <=20)
-	{
-		document.getElementById("")
-	}
-	else if(timeLeft <= 10)
-	{
+		timeleft--;
+		if(timeLeft <=30 && timeLeft > 20)
+		{
+			alert("Hi from a1");
+			document.getElementById("active1").styleSheets.border-color = "green";
+		}
+		else if(timeleft <=20 && timeLeft > 10)
+		{
+			document.getElementById("active2").styleSheets.border-color = "green";
 		
-	}
-	else if(timeleft <= 0)
-	{
-		//End of tracker
-	}
+		}
+		else if(timeLeft <= 10 & timeLeft > 0)
+		{
+			document.getElementById("active3").styleSheets.border-color = "green";
+		}
+		else if(timeleft <= 0)
+		{
+			document.getElementById("active4").styleSheets.border-color = "green";
+		}
 	},delayTime);
 }
